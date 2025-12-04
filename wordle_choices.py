2b4aecee78f473f3c299f6226e9fd366f40ac0f3
akutsner <akutsner@ithaca.edu>
2025-12-04 13:17:01 -0500

    Made it so theres correct letters and used. Need to find a way to change the color of the keys if its used and the output if correct

---
+++ wordle_choices.py
@@ -0,0 +1,33 @@
+wordle_words = {
+    "APPLE": True,
+    "BRAVE": True,
+}
+wordle_words2 = {
+    "APPLE": True,
+    "BRAVE": True,
+    "CHAIR": True,
+    "DELTA": True,
+    "EARTH": True,
+    "FLAME": True,
+    "GRASS": True,
+    "HEART": True,
+    "INDEX": True,
+    "JOKER": True,
+    "KNIFE": True,
+    "LEMON": True,
+    "MIGHT": True,
+    "NORTH": True,
+    "OCEAN": True,
+    "PARTY": True,
+    "QUEEN": True,
+    "ROBOT": True,
+    "SLEEP": True,
+    "TRAIN": True,
+    "ULTRA": True,
+    "VIVID": True,
+    "WATER": True,
+    "XENON": True,
+    "YOUTH": True,
+    "ZEBRA": True
+}
+
