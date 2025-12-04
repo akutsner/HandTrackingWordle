c3148c009a5e54fdddde6fbe32064f41bd54248b
akutsner <akutsner@ithaca.edu>
2025-12-03 20:50:10 -0500

    Put buttons in a different file

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+from buttons import buttons
 
 cap = cv2.VideoCapture(0)
 
@@ -13,82 +14,7 @@ button_cooldown = 0
 pressed_list = []
 
 # Button definitions (defined OUTSIDE the loop)
-buttons = [
-    {"x": 30, "y": 225, "w": 50, "h": 50, "label": "Q", "enabled": False, "color_on": (255, 255, 255),
-     "color_off": (255, 0, 0), "value": "Q", "pressed": False,"delete": False, "quit": False},
-    {"x": 40, "y": 285, "w": 50, "h": 50, "label": "A", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "A", "pressed": False,"delete": False, "quit": False},
-    {"x": 50, "y": 345, "w": 50, "h": 50, "label": "Z", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "Z", "pressed": False,"delete": False, "quit": False},
 
-    {"x": 90, "y": 225, "w": 50, "h": 50, "label": "W", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "W", "pressed": False,"delete": False, "quit": False},
-    {"x": 100, "y": 285, "w": 50, "h": 50, "label": "S", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "S", "pressed": False,"delete": False, "quit": False},
-    {"x": 110, "y": 345, "w": 50, "h": 50, "label": "X", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "X", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 150, "y": 225, "w": 50, "h": 50, "label": "E", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "E", "pressed": False,"delete": False, "quit": False},
-    {"x": 160, "y": 285, "w": 50, "h": 50, "label": "D", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "D", "pressed": False,"delete": False, "quit": False},
-    {"x": 170, "y": 345, "w": 50, "h": 50, "label": "C", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "C", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 210, "y": 225, "w": 50, "h": 50, "label": "R", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "R", "pressed": False,"delete": False, "quit": False},
-    {"x": 220, "y": 285, "w": 50, "h": 50, "label": "F", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "F", "pressed": False,"delete": False, "quit": False},
-    {"x": 230, "y": 345, "w": 50, "h": 50, "label": "V", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "V", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 270, "y": 225, "w": 50, "h": 50, "label": "T", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "T", "pressed": False,"delete": False, "quit": False},
-    {"x": 280, "y": 285, "w": 50, "h": 50, "label": "G", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "G", "pressed": False,"delete": False, "quit": False},
-    {"x": 290, "y": 345, "w": 50, "h": 50, "label": "B", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "B", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 330, "y": 225, "w": 50, "h": 50, "label": "Y", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "Y", "pressed": False,"delete": False, "quit": False},
-    {"x": 340, "y": 285, "w": 50, "h": 50, "label": "H", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "H", "pressed": False,"delete": False, "quit": False},
-    {"x": 350, "y": 345, "w": 50, "h": 50, "label": "N", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "N", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 390, "y": 225, "w": 50, "h": 50, "label": "U", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "U", "pressed": False,"delete": False, "quit": False},
-    {"x": 400, "y": 285, "w": 50, "h": 50, "label": "J", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "J", "pressed": False,"delete": False, "quit": False},
-    {"x": 410, "y": 345, "w": 50, "h": 50, "label": "M", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "M", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 450, "y": 225, "w": 50, "h": 50, "label": "I", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "I", "pressed": False,"delete": False, "quit": False},
-    {"x": 460, "y": 285, "w": 50, "h": 50, "label": "K", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "K", "pressed": False,"delete": False, "quit": False},
-    {"x": 470, "y": 345, "w": 50, "h": 50, "label": " ,", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": ",", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 510, "y": 225, "w": 50, "h": 50, "label": "O", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "O", "pressed": False,"delete": False, "quit": False},
-    {"x": 520, "y": 285, "w": 50, "h": 50, "label": "L", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "L", "pressed": False,"delete": False, "quit": False},
-    {"x": 530, "y": 345, "w": 50, "h": 50, "label": " .", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": ".", "pressed": False,"delete": False, "quit": False},
-
-    {"x": 570, "y": 225, "w": 50, "h": 50, "label": "P", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "P", "pressed": False,"delete": False, "quit": False},
-
-
-    {"x": 170, "y": 405, "w": 290, "h": 50, "label": "Space", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": " ", "pressed": False,"delete": False, "quit": False},
-    {"x": 630, "y": 225, "w": 80, "h": 50, "label": " <-", "enabled": False, "color_on": (0, 255, 0),
-     "color_off": (255, 0, 0), "value": "", "pressed": False, "delete": True, "quit": False},
-
-    {"x": 1200, "y": 10, "w": 80, "h": 80, "label": "QUIT", "enabled": False, "color_on": (255, 0, 0),
-     "color_off": (255, 0, 0), "value": "QUIT", "pressed": False, "delete": False, "quit": True},
-]
 
 
 def get_fingers_down(handLms, h, w):
@@ -123,13 +49,14 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
     index_x = index_y = None
+    text_color = (0, 0, 0)
     # Draw buttons
     for button in buttons:
         color = button["color_on"] if button["enabled"] else button["color_off"]
         cv2.rectangle(img, (button["x"], button["y"]), (button["x"] + button["w"], button["y"] + button["h"]),
                       color, -1)
         cv2.putText(img, f"{button['label']}", (button["x"] + 10, button["y"] + 40), cv2.FONT_HERSHEY_PLAIN, 2,
-                    (255, 255, 255), 2)
+                    text_color, 2)
     # Hand landmarks
     if results.multi_hand_landmarks:
         handLms = results.multi_hand_landmarks[0]
